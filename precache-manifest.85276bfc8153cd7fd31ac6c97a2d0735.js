7bd24c3f92a4af94af6d693e78d15c38a1f5b631
Fristail27 <fristail27@gmail.com>
2020-12-09 21:58:25 +0300

    Updates

---
+++ precache-manifest.85276bfc8153cd7fd31ac6c97a2d0735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77518b71fc68a5cfc5eff02493562c12",
+    "url": "/Homework/index.html"
+  },
+  {
+    "revision": "8ddbd67f5978e0334096",
+    "url": "/Homework/static/css/main.33413d9f.chunk.css"
+  },
+  {
+    "revision": "55b8b4646c1907525ba3",
+    "url": "/Homework/static/js/2.1e5c86fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Homework/static/js/2.1e5c86fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddbd67f5978e0334096",
+    "url": "/Homework/static/js/main.e11aa6ee.chunk.js"
+  },
+  {
+    "revision": "e5323a84600407dfb348",
+    "url": "/Homework/static/js/runtime-main.4d605882.js"
+  }
+]);
