c22989a5dad02e846881edbadddf5a1341e3729a
Fristail27 <fristail27@gmail.com>
2020-12-16 22:14:30 +0300

    Updates

--- precache-manifest.121523e1ce6e523f5849feda5fde5f13.js
+++ precache-manifest.121523e1ce6e523f5849feda5fde5f13.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c13b21b04c61dc5e59d97dbc0de1ef12",
+    "revision": "6298ba4b867bf2a8ce245598f736b65e",
     "url": "/Homework/index.html"
   },
   {
-    "revision": "c71f6ebc3991c81152ee",
+    "revision": "0e2b6a773f8c1f5f8fce",
     "url": "/Homework/static/css/main.b4161ec1.chunk.css"
   },
   {
-    "revision": "aac1aae599a951c01fd6",
-    "url": "/Homework/static/js/2.b8e06b9e.chunk.js"
+    "revision": "10da27493e45c4129a33",
+    "url": "/Homework/static/js/2.aca8e699.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/Homework/static/js/2.b8e06b9e.chunk.js.LICENSE.txt"
+    "url": "/Homework/static/js/2.aca8e699.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c71f6ebc3991c81152ee",
-    "url": "/Homework/static/js/main.400a7a63.chunk.js"
+    "revision": "0e2b6a773f8c1f5f8fce",
+    "url": "/Homework/static/js/main.81d525ea.chunk.js"
   },
   {
     "revision": "e5323a84600407dfb348",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Homework/precache-manifest.e16f1306968fd080185394e125ffb6f8.js"
+  "/Homework/precache-manifest.121523e1ce6e523f5849feda5fde5f13.js"
 );
 
 self.addEventListener('message', (event) => {
